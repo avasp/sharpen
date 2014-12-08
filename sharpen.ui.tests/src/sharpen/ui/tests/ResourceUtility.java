089085b7447231f3fa8ec8893ab5d973447d9b67
avasp <raj041174@yahoo.com>
2014-12-08 11:44:26 +0530

    correction of resource laoding

--- sharpen.ui.tests/src/sharpen/ui/tests/ResourceUtility.java
+++ sharpen.ui.tests/src/sharpen/ui/tests/ResourceUtility.java
@@ -25,18 +25,18 @@ package sharpen.ui.tests;
 
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import sharpen.util.*;
 
 
 public class ResourceUtility {
 	
-	public static String getStringContents(String resourceName) throws IOException {
-		return getStringContents(resourceName, ResourceUtility.class);
-	}
-
-	public static String getStringContents(String resourceName, Class<?> relativeTo) throws IOException {
-		return ResourceLoader.getStringContents(relativeTo, "/" + resourceName);
+	
+	public static String getStringContents(String resourceName) throws IOException {	
+		 byte[] encoded = Files.readAllBytes(Paths.get(resourceName));
+		 return new String(encoded);
 	}	
 
 	public static String getResourceUri(String resourceName) {
