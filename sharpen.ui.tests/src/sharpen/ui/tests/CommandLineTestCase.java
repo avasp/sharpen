88337aa1a72efbd6e0901136e1b137bb8c126eff
avasp <raj041174@yahoo.com>
2014-12-11 19:21:53 +0530

    New test cases enabled

--- sharpen.ui.tests/src/sharpen/ui/tests/CommandLineTestCase.java
+++ sharpen.ui.tests/src/sharpen/ui/tests/CommandLineTestCase.java
@@ -11,23 +11,26 @@ public class CommandLineTestCase extends AbstractConversionTestCase {
 		runResourceTestCaseCMD("annotations/"+ "SimpleAnnotation");
 	}
 	
-	
 	@Test
 	public void testArrayAccess() throws Throwable {
 		runResourceTestCaseCMD("Arrays1");
 	}
+	
 	@Test
 	public void testArrayCreation() throws Throwable {
 		runResourceTestCaseCMD("Arrays2");
 	}
+	
 	@Test
 	public void testArrayInitializer() throws Throwable {
 		runResourceTestCaseCMD("Arrays3");
 	}
+	
 	@Test
 	public void testNestedArrayInitializer() throws Throwable {
 		runResourceTestCaseCMD("Arrays4");
 	}
+	
 	@Test
 	public void testUntypedArrayInitializer() throws Throwable {
 		runResourceTestCaseCMD("Arrays5");
@@ -43,38 +46,29 @@ public class CommandLineTestCase extends AbstractConversionTestCase {
 		runResourceTestCaseCMD("autocasting/Hex");
 	}
 	
-	//@Test
+	@Test
 	public void testConstructorDeclaration() throws Throwable {
 		runResourceTestCaseCMD("Constructors1");
 	}
 		
-	//@Test
+	@Test
 	public void testConstructorInvocationFromConstructor() throws Throwable {
 		runResourceTestCaseCMD("Constructors2");
 	}
 		
-	//@Test
+	@Test
 	public void testInitializers() throws Throwable {
 		runResourceTestCaseCMD("Initializers");
 	}
 	
-	//@Test
-	public void testCollectionMappings() throws Throwable {
-		runResourceTestCaseCMD("CollectionMappings");
-	}
-
+	
 	@Test
 	public void testGenericMethodImpl() throws Throwable {
 		runResourceTestCaseCMD("generics/" + "GenericMethodImpl");
 	}
 	
-	//@Test
-	public void testWildcardTypes() throws Throwable {
-		runResourceTestCaseCMD("generics/" +"WildcardTypes");
-	}
-
 	@Test
 	public void testSimpleInterface() throws Throwable {
 		runResourceTestCaseCMD("generics/" + "GenericInterface");
-	}		
+	}	
 }
